577d40b4d4a040db588495d491d0f76c8aa3aa39
Robert Kumanek <robert.kumanek@gmail.com>
2023-12-17 13:53:38 +0100

    Day 03 - add previous solution as base

---
+++ day_03/task_2/gear_ratios.cpp
@@ -0,0 +1,161 @@
+#include "gear_ratios.hpp"
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+
+std::ostream& operator<<(std::ostream& os, Coordinates coordinates)
+{
+    os << "{" << coordinates.first << ", " << coordinates.second << "}";
+    return os;
+}
+
+bool is_symbol(const char character)
+{
+    return character == '*';
+}
+
+bool are_indexes_in_scope(const EngineSchematic& engine_schematic, int first, int second)
+{
+    return 0 <= first && first < engine_schematic.size() &&
+           0 <= second && second < engine_schematic[0].size();
+}
+
+bool is_number_adjacent_to_symbol(EngineSchematic engine_schematic, Coordinates begin_index, Coordinates end_index)
+{
+
+    if(begin_index.first < engine_schematic.size() -1)
+    {
+        for(int i = begin_index.second - 1; i <= end_index.second + 1; i++)
+        {
+            if(are_indexes_in_scope(engine_schematic, begin_index.first + 1, i) && 
+               is_symbol(engine_schematic[begin_index.first + 1][i]))
+            {
+                return true;
+            }
+        }
+    }
+
+    if(are_indexes_in_scope(engine_schematic, end_index.first, end_index.second + 1) && 
+       is_symbol(engine_schematic[end_index.first][end_index.second + 1]))
+    {
+        return true;
+    }
+
+    if(are_indexes_in_scope(engine_schematic, begin_index.first, begin_index.second - 1) && 
+       is_symbol(engine_schematic[begin_index.first][begin_index.second - 1]))
+    {
+        return true;
+    }
+
+    for(int i = begin_index.second - 1; i <= end_index.second + 1; i++)
+    {
+        if(begin_index.first > 0){
+            if(are_indexes_in_scope(engine_schematic, begin_index.first - 1, i) && 
+               is_symbol(engine_schematic[begin_index.first - 1][i]))
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int find_nr_of_column_of_last_digit_in_sequence(const std::string& row, int begin_index_of_number)
+{
+    bool is_continous_digit_sequence = true;
+
+    int i = begin_index_of_number;
+    for(; i < row.size(); ++i)
+    {
+        if(!std::isdigit(row[i]))
+        {
+            is_continous_digit_sequence = false;
+            break;
+        }
+    }
+
+    return i - 1;
+}
+
+std::optional<std::pair<Coordinates, Coordinates>> ExtractorOfNextNumberIndexes::extract(EngineSchematic engine_schematic)
+{
+
+    for(auto row = next_indexes_.first.first; row < engine_schematic.size(); ++row )
+    {
+        for(auto character = next_indexes_.first.second; character < engine_schematic[row].size(); ++character)
+        {
+            if(std::isdigit(engine_schematic[row][character]))
+            {
+                next_indexes_.second.first = next_indexes_.first.first;
+                next_indexes_.second.second = find_nr_of_column_of_last_digit_in_sequence(engine_schematic[row], next_indexes_.first.second);
+
+                DEBUG_PRINT(next_indexes_.first << " " << next_indexes_.second);
+                auto return_indexes = next_indexes_;
+
+                next_indexes_.first = next_indexes_.second;
+                if(next_indexes_.first.second + 1 != engine_schematic[row].size())
+                {
+                    next_indexes_.first.second++;
+                }
+                else
+                {
+                    next_indexes_.first.first++;
+                    next_indexes_.first.second = 0;
+                }
+                return return_indexes;
+            }
+
+            next_indexes_.first.second++;
+        }
+        next_indexes_.first.first++;
+        next_indexes_.first.second = 0;
+    }
+
+    return std::nullopt;
+}
+
+std::optional<int> ExtractorOfNumberAdjacetToSymbol::extract_next_part_number(EngineSchematic engine_schematic)
+{
+    while(true)
+    {
+        auto number_to_check_indexes =  extractor_.extract(engine_schematic);
+        if(!number_to_check_indexes.has_value())
+        {
+            DEBUG_PRINT("Didn't find any number");
+            return std::nullopt;
+        }
+
+        if(is_number_adjacent_to_symbol(engine_schematic, number_to_check_indexes.value().first, number_to_check_indexes.value().second))
+        {
+            DEBUG_PRINT("")
+            const std::string& row_with_number = engine_schematic[number_to_check_indexes.value().first.first];
+
+            const int& index_of_first_digit_of_number = number_to_check_indexes.value().first.second;
+            const int& index_of_first_last_of_number = number_to_check_indexes.value().second.second;
+            const int part_number{std::stoi(row_with_number.substr(index_of_first_digit_of_number, index_of_first_last_of_number + 1))};
+            DEBUG_PRINT("Founded part number: " << part_number);
+            return std::make_optional<int>(part_number);
+        }
+    }
+}
+
+int ExtractorOfNumberAdjacetToSymbol::sum_part_numbers(EngineSchematic engine_schematic)
+{
+    int total_sum_of_part_numbers{0};
+    while(true)
+    {
+        auto candi_part_number = extract_next_part_number(engine_schematic);
+
+        if(candi_part_number.has_value())
+        {
+            DEBUG_PRINT("candi_part_number: " << candi_part_number.value());
+            total_sum_of_part_numbers += candi_part_number.value();
+            DEBUG_PRINT("total_sum_of_part_numbers: " << total_sum_of_part_numbers);
+        }
+        else
+        {
+            DEBUG_PRINT("total_sum_of_part_numbers: " << total_sum_of_part_numbers);
+            return total_sum_of_part_numbers;
+        }
+    }
+}
---
+++ day_03/task_2/gear_ratios.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <optional>
+#include <iomanip>
+
+const bool kIsDebugOn = true;
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+
+#define DEBUG_PRINT(MSG) if(kIsDebugOn){ std::cout << "[" << std::right << std::setw(16) << __FILE__ << ":" << std::left << std::setw(4) << __LINE__ << "] " << BOLDWHITE << MSG << RESET << std::endl;}
+#define DEBUG_PRINT_TESTNAME(MSG) DEBUG_PRINT(RESET << YELLOW << MSG)
+
+using Coordinates = std::pair<int,int>;
+using EngineSchematic = std::vector<std::string>;
+
+bool is_number_adjacent_to_symbol(EngineSchematic engine_schematic, Coordinates begin_index, Coordinates end_index);
+
+int extract_number(std::string engine_schematic_one_line);
+
+int extract_number_when_symbol_is_adjacent(std::string engine_schematic_one_line);
+
+class ExtractorOfNextNumberIndexes
+{
+public:
+    std::optional<std::pair<Coordinates, Coordinates>> extract(EngineSchematic engine_schematic);
+private:
+    std::pair<Coordinates, Coordinates> next_indexes_{{0, 0}, {0, 0}};
+};
+
+class ExtractorOfNumberAdjacetToSymbol
+{
+public:
+    int sum_part_numbers(EngineSchematic engine_schematic);
+    std::optional<int> extract_next_part_number(EngineSchematic engine_schematic);
+private:
+    ExtractorOfNextNumberIndexes extractor_;
+};
