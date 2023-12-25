d6bb858f9087bc4221e74e44d9681809a91b5360
Robert Kumanek <robert.kumanek@gmail.com>
2023-12-25 21:25:47 +0100

    Day 04 - modify calculate_round_points

--- day_04/scratchcards.cpp
+++ day_04/scratchcards.cpp
@@ -1,4 +1,5 @@
 #include "scratchcards.hpp"
+#include <cmath>
 
 int count_guessed_numbers(const std::set<int>& winning_numbers, std::list<int> scratched_numbers)
 {
@@ -12,6 +13,5 @@ int count_guessed_numbers(const std::set<int>& winning_numbers, std::list<int> s
 int calculate_round_points(const std::set<int>& winning_numbers, std::list<int> scratched_numbers)
 {
     auto guessed_no = count_guessed_numbers(winning_numbers,scratched_numbers);
-
-    return 2*guessed_no;
+    return std::pow(2, --guessed_no);
 }
