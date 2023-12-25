8edd466037251bbcb0b54de57665c51580e3e859
Robert Kumanek <robert.kumanek@gmail.com>
2023-12-25 20:55:55 +0100

    Day 04 - add ut to count_guessed_numbers

--- day_04/main.cpp
+++ day_04/main.cpp
@@ -18,29 +18,31 @@ void run_app()
     }
 
     std::string line;
-    EngineSchematic engine_schematic{};
 
     while(std::getline(fs, line))
     {
-       engine_schematic.push_back(line);
+        std::cout << line << std::endl;
     }
-
-    std::cout << ExtractorOfNumberAdjacentToSymbol{engine_schematic}.sum_gear_ratios() << std::endl;
 
     fs.close();
 }
 
 int main()
 {
-    run_app();
-    // run_tests();
+    // run_app();
+    run_tests();
     return 0;
 }
 
 void run_tests()
 {
     {
-        
+        DEBUG_PRINT_TESTNAME("Test count_guessed_numbers() #1:");
+
+        std::set<int> winning_numbers{41, 48, 83, 86, 17};
+        std::list<int> scratched_numbers{83, 86, 6, 31, 17, 9, 48, 53};
+
+        assert(count_guessed_numbers(winning_numbers, scratched_numbers) == 4);
     }
 
     std::cout << GREEN << "Tests passed!" << RESET << std::endl;
--- day_04/scratchcards.hpp
+++ day_04/scratchcards.hpp
@@ -1,1 +1,16 @@
 #pragma once
+
+#include <set>
+#include <list>
+#include <iomanip>
+
+const bool kIsDebugOn = true;
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+
+#define DEBUG_PRINT(MSG) if(kIsDebugOn){ std::cout << "[" << std::right << std::setw(16) << __FILE__ << ":" << std::left << std::setw(4) << __LINE__ << "] " << BOLDWHITE << MSG << RESET << std::endl;}
+#define DEBUG_PRINT_TESTNAME(MSG) DEBUG_PRINT(RESET << YELLOW << MSG)
