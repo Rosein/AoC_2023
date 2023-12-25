b02ab4dae14794820f4668b3a4592292ba9064b7
Robert Kumanek <robert.kumanek@gmail.com>
2023-12-25 21:16:25 +0100

    Day 04 - add ut calculate_round_points

--- day_04/main.cpp
+++ day_04/main.cpp
@@ -45,6 +45,15 @@ void run_tests()
         assert(count_guessed_numbers(winning_numbers, scratched_numbers) == 4);
     }
 
+    {
+        DEBUG_PRINT_TESTNAME("Test calculate_round_points() #1:");
+
+        std::set<int> winning_numbers{41, 48, 83, 86, 17};
+        std::list<int> scratched_numbers{83, 86, 6, 31, 17, 9, 48, 53};
+
+        assert(calculate_round_points(winning_numbers, scratched_numbers) == 8);
+    }
+
     std::cout << GREEN << "Tests passed!" << RESET << std::endl;
 }
 
