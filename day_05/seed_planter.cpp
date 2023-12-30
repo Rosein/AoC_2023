7ebf20beb0bd2e6e9a5a129e5a1e6cf175c02154
Robert Kumanek <robert.kumanek@gmail.com>
2023-12-30 13:55:26 +0100

    Day 05 - add evaluate_value_of_mapping

--- day_05/seed_planter.cpp
+++ day_05/seed_planter.cpp
@@ -1,1 +1,15 @@
 #include "seed_planter.hpp"
+
+int evaluate_value_of_mapping(int input_to_evaluate, std::vector<MapSubcharacterisctic> map_to_evaluate)
+{
+    for(const auto el : map_to_evaluate)
+    {
+        if(input_to_evaluate >= std::get<kSourceStart>(el) 
+           and input_to_evaluate<=(std::get<kSourceStart>(el) + std::get<kRangeLenght>(el) - 1 ))
+        {
+            return std::get<kDestinationStart>(el) + input_to_evaluate - std::get<kSourceStart>(el) ; 
+        }
+    }
+
+    return input_to_evaluate;
+}
--- day_05/seed_planter.hpp
+++ day_05/seed_planter.hpp
@@ -1,3 +1,17 @@
 #pragma once
 
 #include "../debug_features.hpp"
+#include <tuple>
+#include <vector>
+
+using DestinationRangeStart = int;
+using SourceRangeStart = int;
+using RangeLenght = int;
+
+constexpr int kDestinationStart = 0;
+constexpr int kSourceStart = 1;
+constexpr int kRangeLenght = 2;
+
+using MapSubcharacterisctic = std::tuple<DestinationRangeStart, SourceRangeStart, RangeLenght>;
+
+int evaluate_value_of_mapping(int seed, std::vector<MapSubcharacterisctic> seed_to_soil_map);
