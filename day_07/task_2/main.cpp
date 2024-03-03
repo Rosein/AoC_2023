8feae4b2f6640d955b0994d7450137ef7acf2947
Robert Kumanek <robert.kumanek@gmail.com>
2024-03-03 18:37:47 +0100

    Day_07 - final solution

--- day_07/task_2/main.cpp
+++ day_07/task_2/main.cpp
@@ -11,44 +11,44 @@ void run_tests();
 
 void run_app()
 {
-    // std::string filename{"input"};
-    // std::fstream fs;
-    // fs.open(filename);
-    // if(!fs.is_open())
-    // {
-    //     std::cout << "File couldn't be open" << std::endl;
-    // }
+    std::string filename{"input"};
+    std::fstream fs;
+    fs.open(filename);
+    if(!fs.is_open())
+    {
+        std::cout << "File couldn't be open" << std::endl;
+    }
 
-    // std::string hand;
-    // int bid;
+    std::string hand;
+    int bid;
 
-    // std::map<int, int> hand_of_cards{};
+    std::map<int, int> hand_of_cards{};
 
-    // while(fs >> hand >> bid)
-    // {
-    //     HandOfCards cards = convert_string_hand_to_hand_type(hand);
-    //     int key = transform_to_key(cards);
-    //     hand_of_cards[key] = bid;
-    // }
+    while(fs >> hand >> bid)
+    {
+        HandOfCards cards = convert_string_hand_to_hand_type(hand);
+        int key = transform_to_key(cards);
+        hand_of_cards[key] = bid;
+    }
 
-    // int sum{0};
-    // int rank{1};
+    int sum{0};
+    int rank{1};
 
-    // for (const auto& [key, value] : hand_of_cards)
-    // {
-    //     sum += (value * rank);
-    //     rank++;
-    // }
-    // std::cout << sum << std::endl;
+    for (const auto& [key, value] : hand_of_cards)
+    {
+        sum += (value * rank);
+        rank++;
+    }
+    std::cout << sum << std::endl;
 
-    // fs.close();
+    fs.close();
 }
 
 
 int main()
 {
-    // run_app();
-    run_tests();
+    run_app();
+    // run_tests();
     return 0;
 }
 
