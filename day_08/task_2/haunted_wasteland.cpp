a27beaaf80c7dd5083d026e6fd876c729f28af25
Robert Kumanek <robert.kumanek@gmail.com>
2024-03-08 20:20:25 +0100

    Day 08 - add found_starting_positions

---
+++ day_08/task_2/haunted_wasteland.cpp
@@ -0,0 +1,39 @@
+#include "haunted_wasteland.hpp"
+#include <cassert>
+
+std::string go_to_next_place(const DesertPlan& plan, const std::string& current_position, const char direction)
+{
+    assert(plan.contains(current_position));
+    const auto& next_step_options = plan.at(current_position);
+
+    return direction == 'L' ? next_step_options.first : next_step_options.second;
+}
+
+int count_steps_to_ZZZ(const DesertPlan& desert_plan, const std::string& begin_place, const std::string& directions)
+{
+    int counter_of_steps{0};
+    std::string current_place{begin_place};
+    auto it_dir = directions.begin();
+
+    do {
+        current_place = go_to_next_place(desert_plan, current_place, *it_dir);
+
+        counter_of_steps++;
+        it_dir = std::next(it_dir) == directions.end() ? directions.begin() : std::next(it_dir);
+    } while(current_place != "ZZZ");
+
+    return counter_of_steps;
+}
+
+StartPositions found_starting_positions(const DesertPlan& desert_plan)
+{
+    StartPositions start_positions;
+    for(auto [key, value] : desert_plan)
+    {
+        if(key[2] == 'A')
+        {
+            start_positions.push_back(key);
+        }
+    }
+    return start_positions;
+}
---
+++ day_08/task_2/haunted_wasteland.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../../debug_features.hpp"
+
+#include <map>
+#include <string>
+#include <utility>
+#include <vector>
+
+using DesertPlan = std::map<std::string,std::pair<std::string, std::string>>;
+using StartPositions = std::vector<std::string>;
+
+std::string go_to_next_place(const DesertPlan& plan, const std::string& current_position, const char direction);
+int count_steps_to_ZZZ(const DesertPlan& desert_plan, const std::string& begin_place, const std::string& directions);
+StartPositions found_starting_positions(const DesertPlan& desert_plan);
