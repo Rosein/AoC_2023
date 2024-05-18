2aeac025f11ce78e85a8c365fcf56f8cb9886094
Robert Kumanek <robert.kumanek@gmail.com>
2024-05-18 20:21:01 +0200

    FEAT(day_10): Add find_next_neighbors

--- day_10/task_1/pipe_maze.hpp
+++ day_10/task_1/pipe_maze.hpp
@@ -6,3 +6,5 @@
 using PipeMaze = std::vector<std::string>;
 using MazePoint = std::pair<int, int>;
 MazePoint find_starting_point(const PipeMaze& pipe_maze);
+std::pair<MazePoint, MazePoint> find_next_neighbors(const PipeMaze& pipe_maze,
+                                                    const MazePoint& maze_point);
