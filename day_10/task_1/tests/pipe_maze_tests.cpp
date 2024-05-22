d5bcb88e388562b27172f3b1a10db6998805d795
dariannax3 <38114132+dariannax3@users.noreply.github.com>
2024-05-22 21:39:50 +0200

    TEXT(day_10): add find_next_neighbors tests
    Co-authored-by: Daria Kumanek <dariakumanek@maladie.local>

--- day_10/task_1/tests/pipe_maze_tests.cpp
+++ day_10/task_1/tests/pipe_maze_tests.cpp
@@ -1,9 +1,23 @@
 #include "day_10/task_1/pipe_maze.hpp"
 #include "gtest/gtest.h"
 
-TEST(PipeMazeToolsShould, FindTheStartingPoint)
+
+struct PipeMazeTests : public ::testing::Test 
 {
     const PipeMaze pipe_maze = {"-L|F7", "7S-7|", "L|7||", "-L-J|", "L|-JF"};
+};
 
-    ASSERT_EQ(find_starting_point(pipe_maze), (std::make_pair<int, int>(1, 1)));
+TEST_F(PipeMazeTests, GivenMazeWithPipeLoop_WhenFindTheStartingPoint_ExpectCoordinatesFromFieldWhereS)
+{
+    ASSERT_EQ(find_starting_point(pipe_maze), MazePoint(1, 1));
+}
+
+TEST_F(PipeMazeTests, GivenStartingPipe_WhenFindNextNeighbors_ExpectTwoMazePointsOfConnectedPipes)
+{
+    const MazePoint starting_pipe(find_starting_point(pipe_maze));
+
+    const auto foundNeighbors = find_next_neighbors(pipe_maze, starting_pipe);
+
+    ASSERT_EQ(foundNeighbors.first, MazePoint(1, 2));
+    ASSERT_EQ(foundNeighbors.second, MazePoint(2, 1));
 }
