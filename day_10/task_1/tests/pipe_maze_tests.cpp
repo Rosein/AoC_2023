4da01d6b9e36a7697efba8cabc27eb018727451c
dariannax3 <38114132+dariannax3@users.noreply.github.com>
2024-06-12 18:41:27 +0200

    TEST(day_10): add find_next_neighbors test (#5)
    * TEST(day_10): add find_next_neighbors test
    
    First step: find neighbors for starting_pipe
    Second step: find neighbors for previously found first neighbor
    
    * REFAC(day_10): Simplfy ut with start pipe neigbour
    
    * FEAT(day_10): Extend find_next_neighbors
    
    To handle edge cases:
    - when current pipe is neighbor of starting pipe
    - when current pipe is neighbor of pipe which is turned into the current
      pipe, but the current pipe is not turned into the neighbor
    
    ---------
    
    Co-authored-by: Daria Kumanek <dariakumanek@maladie.local>
    Co-authored-by: Robert Kumanek <robert.kumanek@gmail.com>

--- day_10/task_1/tests/pipe_maze_tests.cpp
+++ day_10/task_1/tests/pipe_maze_tests.cpp
@@ -1,7 +1,6 @@
 #include "day_10/task_1/pipe_maze.hpp"
 #include "gtest/gtest.h"
 
-
 struct PipeMazeTests : public ::testing::Test
 {
     const PipeMaze pipe_maze = {"-L|F7", "7S-7|", "L|7||", "-L-J|", "L|-JF"};
@@ -20,6 +19,18 @@ TEST_F(PipeMazeTests, GivenStartingPipe_WhenFindNextNeighbors_ExpectTwoMazePoint
 
     ASSERT_EQ(foundNeighbors.first, MazePoint(1, 2));
     ASSERT_EQ(foundNeighbors.second, MazePoint(2, 1));
+}
+
+TEST_F(PipeMazeTests, GivenStartingPipe_WhenFindNeighborsForNextPoint_ExpectTwoMazePointsOfConnectedPipes)
+{
+    const MazePoint pipe_which_is_neigbour_of_starting_pipe(MazePoint(1, 2));
+
+    const auto foundNeighborsForSecondPoint =
+        find_next_neighbors(pipe_maze, pipe_which_is_neigbour_of_starting_pipe);
+
+    const MazePoint starting_pipe{MazePoint(1, 1)};
+    ASSERT_EQ(foundNeighborsForSecondPoint.first, starting_pipe);
+    ASSERT_EQ(foundNeighborsForSecondPoint.second, MazePoint(1, 3));
 }
 
 TEST_F(PipeMazeTests, GivenStartingPoint_WhenGoForwardTwoTimesInFirstDirection_ExpectReturnNextTwoCoordinatesFromExpectedDirection)
