1e6b6018c67f703b92082469df38206a0acb44d3
Robert Kumanek <robert.kumanek@gmail.com>
2024-06-29 18:53:27 +0200

    FEAT(day_10): Add solution to main

--- day_10/task_1/main.cpp
+++ day_10/task_1/main.cpp
@@ -16,20 +16,24 @@ void run_app(std::string filename)
     }
 
     std::string line;
+    PipeMaze pipe{};
 
     while(std::getline(fs, line))
     {
-        std::cout << line << std::endl;
+        // std::cout << line << std::endl;
+        pipe.push_back(line);
     }
 
+    int steps = count_steps_to_farthest_point(pipe);
+    std::cout << "steps: " << steps << std::endl;
     fs.close();
 }
 
 
 int main(int argc, char** argv)
 {
-    std::string filename = path_helper::prename +
-        std::string{"/AoC_2023/day_10/task_1/input_example"};
+    std::string filename =
+        path_helper::prename + std::string{"/AoC_2023/day_10/task_1/input"};
     run_app(filename);
     return 0;
 }
