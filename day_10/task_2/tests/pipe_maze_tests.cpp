b46c34d223e06433a23e4c012d2b3cdfbccd8bbd
Robert Kumanek <robert.kumanek@gmail.com>
2024-06-30 17:44:08 +0200

    TEST(day_10): add mark_loop_tiles_in_attributed_maze ut
    Co-authored-by: Daria Kumanek <dariakumanek@maladie.local>

--- day_10/task_2/tests/pipe_maze_tests.cpp
+++ day_10/task_2/tests/pipe_maze_tests.cpp
@@ -3,7 +3,7 @@
 
 struct PipeMazeTests : public ::testing::Test
 {
-    const PipeMaze pipe_maze = {"..F7.", ".FJ|.", "SJ.L7", "|F--J", "LJ..."};
+    const PipeMaze pipe_maze = {"..F7.", ".FJ|.", "SJ.L7", "|F--J", "LJ-.."};
 };
 
 TEST_F(PipeMazeTests, GivenMazeWithPipeLoop_WhenTransformToAttributedMaze_ExpectAdditionalAttributeToAccess)
@@ -19,4 +19,16 @@ TEST_F(PipeMazeTests, GivenMazeWithPipeLoop_WhenTransformToAttributedMaze_Expect
             ASSERT_EQ(maze.check_state_at(MazePoint(i, j)), expected_state);
             ASSERT_EQ(maze.check_pipe_at(MazePoint(i, j)), expected_pipe);
         }
+}
+
+
+TEST_F(PipeMazeTests, GivenMazeWithPipeLoop_WheMarkLoopTilesInAttributedMaze_ExpectTileWhichIsPartOfTheLoopHasStateSetToLoop)
+{
+    AttributedMaze maze{transform_to_attributed_maze(pipe_maze)};
+
+    mark_loop_tiles_in_attributed_maze(maze);
+
+    ASSERT_EQ(maze.check_state_at(MazePoint(0, 3)), State::Loop);
+    ASSERT_EQ(maze.check_state_at(MazePoint(0, 4)), State::Undefined);
+    ASSERT_EQ(maze.check_state_at(MazePoint(4, 2)), State::Undefined);
 }
