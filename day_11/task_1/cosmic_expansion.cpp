e637d54211fd0bcffcac5e78e1e9efdb6f5eb558
Robert Kumanek <robert.kumanek@gmail.com>
2024-08-03 12:11:31 +0200

    FEAT(day_11): Add expand_for_block_without_galaxy

--- day_11/task_1/cosmic_expansion.cpp
+++ day_11/task_1/cosmic_expansion.cpp
@@ -1,4 +1,5 @@
 #include "cosmic_expansion.hpp"
+#include <cassert>
 
 bool CosmicExpansion::does_block_contain_galaxy(const int index, const TypeOfBlock type_of_block)
 {
@@ -23,4 +24,36 @@ bool CosmicExpansion::does_block_contain_galaxy(const int index, const TypeOfBlo
         }
     }
     return false;
+}
+
+std::size_t CosmicExpansion::size_of_rows()
+{
+    return cosmic_universe_.size();
+}
+
+std::size_t CosmicExpansion::size_of_columns()
+{
+    assert(!cosmic_universe_.empty());
+    return cosmic_universe_.front().size();
+}
+
+void CosmicExpansion::expand_for_block_without_galaxy(const int index, const TypeOfBlock type_of_block)
+{
+    assert(!does_block_contain_galaxy(index, type_of_block));
+
+    const char kEmptySpace = '.';
+    if(type_of_block == TypeOfBlock::row)
+    {
+        const auto empty_row = std::string(cosmic_universe_.front().size(), kEmptySpace);
+        const auto where_to_put = std::next(std::begin(cosmic_universe_), index);
+        cosmic_universe_.insert(where_to_put, empty_row);
+
+    } else if(type_of_block == TypeOfBlock::column)
+    {
+        for(std::size_t i = 0U; i < size_of_rows(); ++i)
+        {
+            const int kHowMany = 1;
+            cosmic_universe_[i].insert(index, kHowMany, kEmptySpace);
+        }
+    }
 }
--- day_11/task_1/cosmic_expansion.hpp
+++ day_11/task_1/cosmic_expansion.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include <vector>
+#include <deque>
 #include <string>
 
 enum class TypeOfBlock
@@ -8,15 +8,17 @@ enum class TypeOfBlock
     column
 };
 
-using CosmicUniverse = std::vector<std::string>;
+using CosmicUniverse = std::deque<std::string>;
 
 class CosmicExpansion
 {
 public:
     CosmicExpansion(const CosmicUniverse& cosmic_universe) : cosmic_universe_(cosmic_universe){};
     bool does_block_contain_galaxy(const int index, const TypeOfBlock type_of_block);
+    void expand_for_block_without_galaxy(const int index, const TypeOfBlock type_of_block);
 
+    std::size_t size_of_rows();
+    std::size_t size_of_columns();
 private:
     CosmicUniverse cosmic_universe_;
-
 };
