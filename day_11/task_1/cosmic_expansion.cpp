9b6101f1df1496f7baeb658919f5d9d9b16f380e
Daria Kumanek <dariakumanek@gmail.com>
2024-08-03 12:16:18 +0200

    REFAC(day_11): Add helpers is_row/column
    Replace '!' to 'not'

--- day_11/task_1/cosmic_expansion.cpp
+++ day_11/task_1/cosmic_expansion.cpp
@@ -1,19 +1,29 @@
 #include "cosmic_expansion.hpp"
 #include <cassert>
 
+bool is_row(TypeOfBlock type_of_block)
+{
+    return type_of_block == TypeOfBlock::row;
+}
+
+bool is_column(TypeOfBlock type_of_block)
+{
+    return type_of_block == TypeOfBlock::column;
+}
+
 bool CosmicExpansion::does_block_contain_galaxy(const int index, const TypeOfBlock type_of_block)
 {
     const char kGalaxy = '#';
-    if(type_of_block == TypeOfBlock::row)
+    if(is_row(type_of_block))
     {
-        for(std::size_t i = 0; i < cosmic_universe_[0].size(); ++i)
+        for(std::size_t i = 0; i < cosmic_universe_.front().size(); ++i)
         {
             if(cosmic_universe_[index][i] == kGalaxy)
             {
                 return true;
             }
         }
-    } else if(type_of_block == TypeOfBlock::column)
+    } else if(is_column(type_of_block))
     {
         for(std::size_t i = 0; i < cosmic_universe_.size(); ++i)
         {
@@ -33,22 +43,22 @@ std::size_t CosmicExpansion::size_of_rows()
 
 std::size_t CosmicExpansion::size_of_columns()
 {
-    assert(!cosmic_universe_.empty());
+    assert(not cosmic_universe_.empty());
     return cosmic_universe_.front().size();
 }
 
 void CosmicExpansion::expand_for_block_without_galaxy(const int index, const TypeOfBlock type_of_block)
 {
-    assert(!does_block_contain_galaxy(index, type_of_block));
+    assert(not does_block_contain_galaxy(index, type_of_block));
 
     const char kEmptySpace = '.';
-    if(type_of_block == TypeOfBlock::row)
+    if(is_row(type_of_block))
     {
         const auto empty_row = std::string(cosmic_universe_.front().size(), kEmptySpace);
         const auto where_to_put = std::next(std::begin(cosmic_universe_), index);
         cosmic_universe_.insert(where_to_put, empty_row);
 
-    } else if(type_of_block == TypeOfBlock::column)
+    } else if(is_column(type_of_block))
     {
         for(std::size_t i = 0U; i < size_of_rows(); ++i)
         {
