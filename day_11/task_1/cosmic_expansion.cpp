4f9462a3b84274d7550c8b34d2598a6b60ae30a0
Robert Kumanek <robert.kumanek@gmail.com>
2024-08-03 11:10:34 +0200

    REFAC(day_11): Refactor does_block_contain_galaxy
    1. Name magic value
    2. Change postincrementation into preincrementation
    3. Add else keyword

--- day_11/task_1/cosmic_expansion.cpp
+++ day_11/task_1/cosmic_expansion.cpp
@@ -2,21 +2,21 @@
 
 bool CosmicExpansion::does_block_contain_galaxy(const int index, const TypeOfBlock type_of_block)
 {
+    const char kGalaxy = '#';
     if(type_of_block == TypeOfBlock::row)
     {
-        for(std::size_t i = 0; i < cosmic_universe_[0].size();  i++)
+        for(std::size_t i = 0; i < cosmic_universe_[0].size(); ++i)
         {
-            if(cosmic_universe_[index][i] == '#')
+            if(cosmic_universe_[index][i] == kGalaxy)
             {
                 return true;
             }
         }
-    }
-    if(type_of_block == TypeOfBlock::column)
+    } else if(type_of_block == TypeOfBlock::column)
     {
-        for(std::size_t i = 0; i < cosmic_universe_.size(); i++)
+        for(std::size_t i = 0; i < cosmic_universe_.size(); ++i)
         {
-            if(cosmic_universe_[i][index] == '#')
+            if(cosmic_universe_[i][index] == kGalaxy)
             {
                 return true;
             }
