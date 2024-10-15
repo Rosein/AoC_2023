90a321bf34c220bd5bff1428f5977708830fcaf2
dariannax3 <38114132+dariannax3@users.noreply.github.com>
2024-10-15 20:41:13 +0200

    TEST(day_12): Add ut to develop arrange spring (#23)
    * TEST(day_12): Add ut to develop arrange spring
    
    * FEAT/TEST(day_12): Add full solution
    
    ---------
    
    Co-authored-by: Robert Kumanek <robert.kumanek@gmail.com>
    Co-authored-by: Daria Kumanek <dariakumanek@maladie.local>

--- day_12/task_1/hot_springs.hpp
+++ day_12/task_1/hot_springs.hpp
@@ -4,4 +4,8 @@
 
 std::vector<int> extract_groups(std::string spring_condition_data);
 std::string extract_conditions(std::string spring_condition_data);
-std::string arrange_spring(std::string extracted_condition, std::vector<int> extracted_spring_groups);
+// std::string arrange_spring(std::string extracted_condition, std::vector<int> extracted_spring_groups);
+int arrange_spring(std::string extracted_condition, std::vector<int> extracted_spring_groups);
+int check_last_condition(std::string extracted_condition);
+
+bool fits(std::string extracted_condition, int start, int end);
